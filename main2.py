e4d43208d33534e52428ce87cf3527c432f6f985
Олексей <whoisgodlike@icloud.com>
2025-12-27 14:12:37 +0300

    ujdyj

---
+++ main2.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from database import engine, Base
+import auth
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    print("--- БАЗА ДАННЫХ ПОДКЛЮЧЕНА ---")
+    yield
+
+app = FastAPI(lifespan=lifespan, title="Olympiad Platform")
+app.include_router(auth.router)
+
+@app.get("/")
+def root():
+    return {"message": "Server is running via Modules!"}
