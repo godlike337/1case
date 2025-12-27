5278938cc949b9a8a37ffc177103df6882553a7f
Олексей <whoisgodlike@icloud.com>
2025-12-27 16:46:50 +0300

    task klass

--- models.py
+++ models.py
@@ -8,3 +8,11 @@ class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column() #хеш
     # role: Mapped[str] = mapped_column(default="user") задел
+class Task(Base):
+        __tablename__ = "tasks"
+
+        id: Mapped[int] = mapped_column(primary_key=True)
+        title: Mapped[str] = mapped_column()  # Название
+        description: Mapped[str] = mapped_column()  # Текст задачи
+        difficulty: Mapped[int] = mapped_column()  # Сложность (1-10)
+        correct_answer: Mapped[str] = mapped_column()  # Правильный ответ (скрыт)
